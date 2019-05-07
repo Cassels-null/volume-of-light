fd36a27aa562159650c2ecb186148b4304b9b7f8
Cassels-null <42cassels@gmail.com>
2019-05-06 21:58:29 -0400

    Relocate bundle file

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
 
   entry: './client/index.jsx',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'client/dist'),
     filename: 'bundle.js'
   },
   module: {
